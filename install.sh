220aa99e77335829fd5262df0bf16b9cd0761a34
Eric Buckley <eric.buckley@gmail.com>
2012-07-13 03:13:08 +0000

    updated install.sh to work with submodules

--- install.sh
+++ install.sh
@@ -1,16 +1,45 @@
-#!/bin/sh
+#!/bin/bash
 
-BASEDIR=$(dirname $0)
+cd "${HOME}/.dotfiles"
 
-ln -s $BASEDIR/bashrc ~/.bashrc
-ln -s $BASEDIR/bin ~/.bin
-ln -s $BASEDIR/gitconfig ~/.gitconfig
-ln -s $BASEDIR/gvimrc ~/.gvimrc
-ln -s $BASEDIR/hgrc ~/.hgrc
-ln -s $BASEDIR/jshintrc ~/.jshintrc
-ln -s $BASEDIR/pdbrc ~/.pdbrc
-ln -s $BASEDIR/pythonrc.py ~/.pythonrc.py
-ln -s $BASEDIR/tmux.conf ~/.tmux.conf
-ln -s $BASEDIR/vimrc ~/.vimrc
+# Pull down the latest changes
+# git pull origin master
 
-$BASEDIR/vim/plugins.sh
+# Check out submodules
+git submodule --quiet update --init
+
+cd "${OLDPWD}"
+
+function mirrorfiles() {
+    # Copy .gitconfig
+    # Any global git commands in ~/.extra will be written to .gitconfig
+    # This prevents them being committed to the repository
+    rsync -avz --quiet ${HOME}/.dotfiles/git/gitconfig  ${HOME}/.gitconfig
+
+    # Symlink everything else
+    # bash_profile sources other files from the repository
+    # Force remove the vim directory if it is already there
+    if [ -e "${HOME}/.vim" ]; then
+        rm -rf "${HOME}/.vim"
+    fi
+    ln -fs ".dotfiles/bashrc"             "${HOME}/.bashrc"
+    ln -fs ".dotfiles/bin"                "${HOME}/.bin"
+    ln -fs ".dotfiles/gitconfig"          "${HOME}/.gitconfig"
+    ln -fs ".dotfiles/hgrc"               "${HOME}/.hgrc"
+    ln -fs ".dotfiles/jshintrc"           "${HOME}/.jshintrc"
+    ln -fs ".dotfiles/pdbrc"              "${HOME}/.pdbrc"
+    ln -fs ".dotfiles/pythonrc.py"        "${HOME}/.pythonrc.py"
+    ln -fs ".dotfiles/tmux.conf"          "${HOME}/.tmux.conf"
+    ln -fs ".dotfiles/vim"                "${HOME}/.vim"
+    ln -fs ".dotfiles/vim/gvimrc"         "${HOME}/.gvimrc"
+    ln -fs ".dotfiles/vim/vimrc"          "${HOME}/.vimrc"
+
+    echo "Dotfiles update complete"
+}
+
+read -p "This will overwrite some existing files in your home directory. Are you sure? (y/n) " -n 1
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    mirrorfiles
+    source ~/.bashrc
+fi
