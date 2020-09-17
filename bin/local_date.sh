b82b288c4e59c8e7550480bba063e6b6e2150e88
Eric Buckley <eric.buckley@gmail.com>
2020-09-16 20:24:59 -0700

    removing unused dotfiles and scripts

--- bin/local_date.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ ! -z $1 ]
-then
-    FORMAT=$1
-else
-    FORMAT="%d %b"
-fi
-date +"$FORMAT"
--- bin/local_time.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ ! -z $1 ]
-then
-    FORMAT=$1
-else
-    FORMAT="%R"
-fi
-date +"$FORMAT"
