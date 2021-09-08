df99814831b6e7e01103e7d30577f7b4798025b3
Eric Buckley <eric.buckley@gmail.com>
2021-09-07 19:59:25 -0700

    removing ahh

--- oh-my-zsh/.oh-my-zsh/custom/plugins/tmux-proj/tmux-proj.plugin.zsh
+++ oh-my-zsh/.oh-my-zsh/custom/plugins/tmux-proj/tmux-proj.plugin.zsh
@@ -1,29 +1,19 @@
 function ah () {
     cd $HOME/projects/adhoc/code/$1
-    tmux attach -t $1 || tmux new-session -s $1 -c $PWD
+    tmux new-session -A -s $1 -c $PWD
     git s
-}
-
-function ahh () {
-    NUM=$(basename -s .zip $1)
-    DIR=$HOME/Downloads/homework/$NUM
-    mkdir -p $DIR
-    mv $1 $DIR
-    unzip $DIR/${NUM}.zip -d $DIR/
-    cd $DIR
-    tmux attach -t homework -c $DIR || tmux new-session -s homework -c $DIR
 }
 
 function wwd () {
     cd $HOME/projects/wwd/code/wwd
-    tmux attach -t wwd || tmux new-session -s wwd
+    tmux new-session -A -s wwd
     workon wwd
     git s
 }
 
 function jbf () {
     cd $HOME/projects/jbf/jbf
-    tmux attach -t jbf || tmux new-session -s jbf
+    tmux new-session -A -s jbf
     workon jbf
     git s
 }
