648156ae45ed6262a332f221724b751cee275b42
Eric Buckley <eric.buckley@gmail.com>
2021-09-07 19:59:57 -0700

    adding popuptmux script

---
+++ tmux/.tmux/popuptmux.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+if [ "$(tmux display-message -p -F "#{session_name}")" = "scratch" ];then
+    tmux detach-client
+else
+    tmux display-popup -E "tmux new-session -A -s scratch"
+fi
