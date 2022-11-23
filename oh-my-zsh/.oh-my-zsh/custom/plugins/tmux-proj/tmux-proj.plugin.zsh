edc7e06efb6a4c8d32aa6c6e6acfc406541d5cd4
Eric Buckley <eric.buckley@gmail.com>
2022-11-22 20:24:21 -0800

    adding testing for tmux-proj to make sure repo exists

--- oh-my-zsh/.oh-my-zsh/custom/plugins/tmux-proj/tmux-proj.plugin.zsh
+++ oh-my-zsh/.oh-my-zsh/custom/plugins/tmux-proj/tmux-proj.plugin.zsh
@@ -1,19 +1,17 @@
 function ah () {
-    cd $HOME/repos/$1
-    tmux new-session -A -s $1 -c $PWD
-    git s
+    if [ ! -d "$HOME/repos/$1" ]
+    then
+        echo "$HOME/repos/$1 does not exist" >&2
+    else
+        cd $HOME/repos/$1
+        tmux new-session -A -s $1 -c $PWD
+        git s
+    fi
 }
 
 function wwd () {
     cd $HOME/projects/wwd/code/wwd
     tmux new-session -A -s wwd
     workon wwd
-    git s
-}
-
-function jbf () {
-    cd $HOME/projects/jbf/jbf
-    tmux new-session -A -s jbf
-    workon jbf
     git s
 }
