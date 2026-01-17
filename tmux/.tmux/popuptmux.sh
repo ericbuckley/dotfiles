e0f1efc1eff2585da68cef59651c3baf3d1c8203
Eric Buckley <eric.buckley@gmail.com>
2026-01-17 15:40:02 -0800

    fix space

--- tmux/.tmux/popuptmux.sh
+++ tmux/.tmux/popuptmux.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-if [ "$(tmux display-message -p -F "#{session_name}")" = "scratch" ];then
+if [ "$(tmux display-message -p -F "#{session_name}")" = "scratch" ]; then
     tmux detach-client
 else
     tmux display-popup -E "tmux new-session -A -s scratch"
