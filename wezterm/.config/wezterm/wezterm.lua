19b9e3eb46c41394c9b599f6b29bf198042b6850
Eric Buckley <eric.buckley@gmail.com>
2026-05-10 11:42:26 -0700

    adaptive themes in wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -19,10 +19,20 @@ config.font = wezterm.font_with_fallback({
   },
 })
 
+local theme_map = {
+  ["Light"] = "Solarized Dark (Gogh)",
+  ["Dark"]  = "Solarized Dark Higher Contrast (Gogh)",
+}
 config.font_size = 16.0
-config.color_scheme = "Solarized Dark - Patched"
-
 config.front_end = "WebGpu"
 config.window_decorations = "RESIZE"
+config.enable_kitty_keyboard = true
+
+-- Default to dark if we can't determine the state
+local appearance = wezterm.gui and wezterm.gui.get_appearance() or "Dark"
+config.color_scheme = theme_map[appearance]
+config.set_environment_variables = {
+  TERM_APPEARANCE = appearance:lower(),
+}
 
 return config
